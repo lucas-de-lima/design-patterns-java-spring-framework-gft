9112dbea02c2c8e974d5d577af4858e912746231
lucas-de-lima <lucasramosdelima@gmail.com>
2024-08-19 19:19:10 -0300

    fix: import GenerationType

--- src/main/java/com/lucasdelima/dpj/model/Cliente.java
+++ src/main/java/com/lucasdelima/dpj/model/Cliente.java
@@ -1,9 +1,6 @@
 package com.lucasdelima.dpj.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 
 @Entity
 public class Cliente {
