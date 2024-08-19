3fcc16c155c1af6fde170452ca25026c1840bd2a
lucas-de-lima <lucasramosdelima@gmail.com>
2024-08-19 18:58:19 -0300

    feat: create class Cliente

---
+++ src/main/java/com/lucasdelima/dpj/model/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.lucasdelima.dpj.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+
+}
