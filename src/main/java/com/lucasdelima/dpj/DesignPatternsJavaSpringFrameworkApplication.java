21ea2b4914b192691ad1835edf93d309c6f01275
lucas-de-lima <lucasramosdelima@gmail.com>
2024-08-19 18:57:48 -0300

    feat: create project

---
+++ src/main/java/com/lucasdelima/dpj/DesignPatternsJavaSpringFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.lucasdelima.dpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsJavaSpringFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternsJavaSpringFrameworkApplication.class, args);
+	}
+
+}
